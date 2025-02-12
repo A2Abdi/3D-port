e38af3feafabddf76a4170674d596607db6163f9
A2Abdi <hj2182@wayne.edu>
2025-02-12 12:57:45 -0500

    deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3D-port/', // Set base to your repository name
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf']
 })
